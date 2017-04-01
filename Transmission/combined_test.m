5c4fd2385b9c293e5e4b6a91af8b37cd9332f598
Sachin Leelasena <psachinl@users.noreply.github.com>
2017-04-01 17:32:19 +0100

    Integrated shortest path map based movement in a 2D plane, added TODO comments for next steps in simulation

--- Transmission/combined_test.m
+++ Transmission/combined_test.m
@@ -63,33 +63,60 @@ end
 
 clear node
 
-% Transmit message packets
+% Loop over each time slice t
+for t=1:100
+    
+    % Transmit message packets
 
-% Transmission occurs if a given node has a message to transmit, if the
-% destination node is within BLE range and if the destination node has not
-% received the message already. If the destination node already has the
-% message, the message tables are updated by the destination node sending
-% an update packet to the source node
+    % Transmission occurs if a given node has a message to transmit, if the
+    % destination node is within BLE range and if the destination node has not
+    % received the message already. If the destination node already has the
+    % message, the message tables are updated by the destination node sending
+    % an update packet to the source node
 
-for src=1:number_of_nodes
-    if nodes{src}.message_to_transmit
-        for dest=1:number_of_nodes
-            if dest ~= src
-                if nodes{src}.checkBTRange(nodes{dest}) && ~nodes{dest}.message_to_transmit
-                    [nodes{src},nodes{dest}] = nodes{src}.transmit(nodes{dest});
-                elseif nodes{src}.checkBTRange(nodes{dest}) && nodes{dest}.message_to_transmit
-                    nodes{src}.message_table{dest} = true;
-                    nodes{src}.table_updates = nodes{src}.table_updates + 1;
-                    nodes{dest}.update_packets_transmitted = nodes{dest}.update_packets_transmitted + 1;
+    for src=1:number_of_nodes
+        if nodes{src}.message_to_transmit
+            for dest=1:number_of_nodes
+                if dest ~= src
+                    if nodes{src}.checkBTRange(nodes{dest}) && ~nodes{dest}.message_to_transmit
+                        [nodes{src},nodes{dest}] = nodes{src}.transmit(nodes{dest});
+                    elseif nodes{src}.checkBTRange(nodes{dest}) && nodes{dest}.message_to_transmit
+                        nodes{src}.message_table{dest} = true;
+                        nodes{src}.table_updates = nodes{src}.table_updates + 1;
+                        nodes{dest}.update_packets_transmitted = nodes{dest}.update_packets_transmitted + 1;
+                    end
                 end
             end
         end
     end
+
+    clear src dest
+    
+    % Move nodes to next position in path and update tables
+    
+    for n=1:number_of_nodes
+        % Move node to next position and update location table
+        nodes{n}.current_position = [nodes{n}.position{4}(t+1,1),nodes{n}.position{4}(t+1,2)];
+        nodes{n}.location_table{n} = nodes{n}.current_position;
+        nodes{n}.table_updates = nodes{n}.table_updates + 1;
+        
+        % Send update packets to nearby nodes to update their location
+        % tables
+        for k=1:number_of_nodes
+            if k ~= n && nodes{n}.checkBTRange(nodes{k})
+                nodes{n}.update_packets_transmitted = nodes{n}.update_packets_transmitted + 1;
+                nodes{k}.location_table{n} = nodes{n}.current_position;
+                nodes{k}.table_updates = nodes{k}.table_updates + 1;
+            end
+        end
+    end
+    
 end
 
-clear src dest
+clear t n k
 
-% TODO: Step through path
-% TODO: If the node has moved, transmit update messages to neighbouring
-% nodes
 % TODO: Add ack packets if required
+% TODO: Add stationary nodes (station, bus stop etc.) which have a message
+% to transmit
+% TODO: Add path recalculation when a message is received from a stationary
+% node. The nodes should then move along the new path to a new destination

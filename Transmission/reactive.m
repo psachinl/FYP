49deea9369302af6e5e29577f688067a7d1bb3aa
Sachin Leelasena <psachinl@users.noreply.github.com>
2017-04-11 16:43:58 +0100

    Created class for reactive transmission nodes, started removing active routing behaviour from test script

--- Transmission/reactive.m
+++ Transmission/reactive.m
@@ -2,7 +2,7 @@ clear
 close all
 
 number_of_moving_groups = 3;
-nodes_per_group = 5;
+nodes_per_group = 2;
 number_of_stationary_nodes = 3;
 number_of_moving_nodes = number_of_moving_groups * nodes_per_group;
 number_of_nodes = number_of_stationary_nodes + number_of_moving_nodes;
@@ -36,45 +36,17 @@ for m = 1:number_of_moving_groups
     for n = 1:nodes_per_group
         index = n + (m-1)*nodes_per_group + number_of_stationary_nodes;
         
-        nodes{index} = DREAMNode;
+        nodes{index} = ReactiveNode;
         nodes{index}.id = index;
         nodes{index}.group = m;
         nodes{index}.start_point = [map_node_positions(start_node(m),1),map_node_positions(start_node(m),2)];
         nodes{index}.end_point = [map_node_positions(end_node(m),1),map_node_positions(end_node(m),2)];
         nodes{index}.min_speed = min_speed(m);
         nodes{index}.max_speed = max_speed(m);
-        nodes{index}.location_table{1,number_of_nodes} = [];
-        nodes{index}.message_table{1,number_of_nodes} = [];
         nodes{index}.current_position = nodes{index}.start_point;
-
-        % Positions of stationary nodes are fixed and therefore known at start
-        for k=1:number_of_stationary_nodes
-            nodes{index}.location_table{k} = nodes{k}.current_position;
-        end
     end
 end
-clear m n k
-
-% Initialise nodes
-
-% Update packets contain new position and/or new message status values
-
-for i=1+number_of_stationary_nodes:number_of_nodes
-    for j=1+number_of_stationary_nodes:number_of_nodes
-        if nodes{i}.checkBTRange(nodes{j}) && i~=j
-            nodes{i}.location_table{j} = nodes{j}.current_position;
-            nodes{i}.message_table{j} = nodes{j}.message_to_transmit;
-            nodes{i}.table_updates = nodes{i}.table_updates + 1;
-            nodes{j}.update_packets_transmitted = nodes{j}.update_packets_transmitted + 1;
-        elseif i == j % Node updates it's own table entries, no need for packet transmission
-            nodes{i}.location_table{i} = nodes{i}.current_position;
-            nodes{i}.message_table{i} = nodes{i}.message_to_transmit;
-            nodes{i}.table_updates = nodes{i}.table_updates + 1;
-        end
-    end
-end
-
-clear i j
+clear m n
 
 % Calculate path for each moving node
 
@@ -96,52 +68,22 @@ for node = 1+number_of_stationary_nodes:number_of_nodes
     end
 end
 
-clear node
+clear node plot_path
 
 % Loop over each time slice t
 for t=1:max_time-1
     
     % Transmit message packets
 
-    % Transmission occurs if a given node has a message to transmit, if the
-    % destination node is within BLE range and if the destination node has not
-    % received the message already. If the destination node already has the
-    % message, the message tables are updated by the destination node sending
-    % an update packet to the source node
+    % The reactive routing algorithm floods the channel with message
+    % packets to transmit the message
     
     % Stationary node to moving node transmission step
     for src=1:number_of_stationary_nodes
         if nodes{src}.message_to_transmit
-            for dest=1+number_of_stationary_nodes:number_of_nodes
-                if nodes{src}.checkBTRange(nodes{dest}) && ~nodes{dest}.message_to_transmit
-                    [nodes{src},nodes{dest}] = nodes{src}.transmit(nodes{dest});
-
-                    % Remove duplicates that were added to avoid indexing
-                    % errors
-                    nodes{dest} = removeDuplicates(nodes{dest});
-                    
-                    % Calculate path to new exit point, update node
-                    % end_point
-                    [start_and_end,waypoints,main_path] = SPMBM(edge_start_points,edge_end_points,edge_weights,end_node(nodes{dest}.group),new_exit_point,nodes{dest}.min_speed,nodes{dest}.max_speed,map_node_positions);
-                    start_and_end(1,:) = nodes{dest}.start_point; % Keep original start point
-                    nodes{dest}.end_point = start_and_end(2,:);
-                    
-                    % Append overall path to existing path
-                    overall_path = [main_path; start_and_end(end,:)];
-                    nodes{dest}.position{4} = [nodes{dest}.position{4};overall_path];
-                    
-                    % Update remaining position cells
-                    nodes{dest}.position{1} = start_and_end;
-                    nodes{dest}.position{2} = [nodes{dest}.position{2};waypoints];
-                    nodes{dest}.position{3} = [nodes{dest}.position{3};main_path];
-                    
-                    if debug
-                        fprintf('Time = %d \n',t);
-                        fprintf('Node %d position changed after transmission from node %d \n',dest,src);
-                        fprintf('Node %d position{4} length = %d \n',dest,length(nodes{dest}.position{4}));
-                    end
-                end
-            end
+            % Source node floods channel with packets
+            % If a destination node is within BLE range, it picks up the
+            % message
         end
     end
     
@@ -150,57 +92,7 @@ for t=1:max_time-1
     % Moving node to moving node transmission step
     for src=1+number_of_stationary_nodes:number_of_nodes
         if nodes{src}.message_to_transmit
-            for dest=1+number_of_stationary_nodes:number_of_nodes
-                if dest ~= src
-                    if nodes{src}.checkBTRange(nodes{dest}) && ~nodes{dest}.message_to_transmit
-                        if debug
-                            fprintf('Time = %d \n',t);
-                            fprintf('Transmitting from node %d to %d \n',src,dest);
-                            fprintf('Node %d position = [%d,%d] \n',src,nodes{src}.current_position(1),nodes{src}.current_position(2));
-                            fprintf('Node %d position = [%d,%d] \n',dest,nodes{dest}.current_position(1),nodes{dest}.current_position(2));
-                        end
-                        
-                        [nodes{src},nodes{dest}] = nodes{src}.transmit(nodes{dest});
-                        
-                        % Calculate path to nearest waypoint
-                        [~,waypoints_w,main_path_w,overall_path_w,nearest_waypoint] = move2Waypoint(nodes{dest},map_node_positions);
-                        
-                        % Calculate path from nearest waypoint to new exit
-                        % point
-                        [start_and_end,waypoints,main_path] = SPMBM(edge_start_points,edge_end_points,edge_weights,nearest_waypoint,new_exit_point,nodes{dest}.min_speed,nodes{dest}.max_speed,map_node_positions);
-                        overall_path = [start_and_end(1,:); main_path; start_and_end(end,:)];
-                        
-                        % Combine paths to find overall path, update
-                        % end_point
-                        start_and_end(1,:) = nodes{dest}.start_point; % Keep original start point
-                        nodes{dest}.end_point = start_and_end(2,:);
-                        waypoints = [waypoints_w;waypoints];
-                        main_path = [main_path_w;main_path];
-                        overall_path = [overall_path_w;overall_path];
-
-                        % Remove duplicates that were added to avoid indexing
-                        % errors
-                        nodes{dest} = removeDuplicates(nodes{dest});
-                        
-                        % Truncate paths so remaining steps are removed
-                        nodes{dest}.position{4} = nodes{dest}.position{4}(1:t,:);
-                        nodes{dest}.position{3} = nodes{dest}.position{3}(1:t-1,:);
-                        
-                        % Append new path to existing node path
-                        nodes{dest}.position{4} = [nodes{dest}.position{4};overall_path];
-                    
-                        % Update remaining position cells
-                        nodes{dest}.position{1} = start_and_end;
-                        nodes{dest}.position{2} = [nodes{dest}.position{2};waypoints];
-                        nodes{dest}.position{3} = [nodes{dest}.position{3};main_path];
-
-                    elseif nodes{src}.checkBTRange(nodes{dest}) && nodes{dest}.message_to_transmit
-                        nodes{src}.message_table{dest} = true;
-                        nodes{src}.table_updates = nodes{src}.table_updates + 1;
-                        nodes{dest}.update_packets_transmitted = nodes{dest}.update_packets_transmitted + 1;
-                    end
-                end
-            end
+            
         end
     end
 
@@ -209,10 +101,8 @@ for t=1:max_time-1
     % Move nodes to next position in path and update tables
     
     for n=1+number_of_stationary_nodes:number_of_nodes
-        % Move node to next position and update location table
+        % Move node to next position
         nodes{n}.current_position = [nodes{n}.position{4}(t+1,1),nodes{n}.position{4}(t+1,2)];
-        nodes{n}.location_table{n} = nodes{n}.current_position;
-        nodes{n}.table_updates = nodes{n}.table_updates + 1;
         
         % Send update packets to nearby nodes to update their location
         % tables
@@ -228,5 +118,3 @@ for t=1:max_time-1
 end
 
 clear t n k
-
-% TODO: Add ack packets if required to model link failure

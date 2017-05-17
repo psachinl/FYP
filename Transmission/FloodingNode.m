b9234eac21b87b083ef2d115cb8f998a14af02c1
Sachin Leelasena <psachinl@users.noreply.github.com>
2017-05-17 19:29:04 +0100

    Started working on a flooding based algorithm

---
+++ Transmission/FloodingNode.m
@@ -0,0 +1,75 @@
+classdef FloodingNode
+    % Class to describe nodes in a MANET where transmission is via flooding
+    
+    % Constant object values
+    properties (Constant)
+        max_transmission_distance = 10; % Bluetooth transmission max 10m
+        transmission_cost = 0.5;        % Power consumption in Watts
+        table_update_cost = 0.25;
+        transmission_speed = 5;
+    end
+    
+    % Public variables
+    properties
+        id
+        group
+        position = {};
+        current_position
+        start_point
+        end_point
+        min_speed
+        max_speed
+        packets_transmitted = 0;
+        packets_received = 0;
+        message_to_transmit = false;
+        location_table
+        message_table
+        update_packets_transmitted = 0;
+        table_updates = 0;
+    end
+    
+    % Private variables
+    properties (GetAccess=private)
+        transmission_required
+    end
+    
+    % Values that are only calculated when required
+    properties (Dependent)
+        power_consumption
+    end
+    
+    % Public methods
+    methods
+        function power_consumption=get.power_consumption(self)
+            % Method to calculate power consumption
+            power_consumption = (self.packets_transmitted + self.update_packets_transmitted) * self.transmission_cost + self.table_updates * self.table_update_cost;
+        end
+        
+        function [self,dst] = transmit(self,dst)
+            % Method to transmit a message from node to dst node
+            self.packets_transmitted = self.packets_transmitted + 1;
+            dst.packets_received = dst.packets_received + 1;
+            dst.message_to_transmit = true;
+            dst.message_table{dst.id} = true;
+            dst.message_table{self.id} = true;
+            self.message_table{dst.id} = true;
+        end
+        
+        function inBTRange = checkBTRange(self,node2)
+        % Function to check whether two nodes are within Bluetooth 
+        % transmission range
+
+        % Inputs: Transmission node objects
+        % Output: Boolean true if nodes are in range
+
+            dist = calculateDistance(self,node2);
+
+            if dist <= 10 % 10m range for Class 2 BLE radios
+                inBTRange = true;
+            else
+                inBTRange = false;
+            end
+        end
+    end
+end
+        
---
+++ Transmission/flooding.m
@@ -0,0 +1,58 @@
+clear
+
+clear
+close all
+
+number_of_moving_groups = 3;
+nodes_per_group = 2;
+number_of_stationary_nodes = 3;
+number_of_moving_nodes = number_of_moving_groups * nodes_per_group;
+number_of_nodes = number_of_stationary_nodes + number_of_moving_nodes;
+max_time = 400;
+debug = true; % If true, text printed to console
+
+nodes{1,number_of_nodes} = []; % Cell array to store all nodes
+edge_start_points = [1 3 3 2 6 1 7 4 7 8];
+edge_end_points =   [3 4 5 6 7 2 6 5 8 7];
+edge_weights = [579 40 128 267 163 250 0 115 18 0];
+start_node = [1,2,1,2]; % Array of start points for each group
+end_node = [4,8,5,8]; % End points
+plot_path = 0; % Whether to plot (1) the movement or not (0)
+min_speed=[1,1,1,1]; % Min and max speeds for each group
+max_speed=[2,2,2,2];
+map_node_positions = [340,440; 267,181; 340,919; 360,1000; 400,1000; 0,181; 0,18; 0,0];
+
+% Set initial values for each stationary node 
+for n = 1:number_of_stationary_nodes
+    nodes{n} = StationaryNode;
+    nodes{n}.id = n;
+    nodes{n}.current_position = [map_node_positions(end_node(n),1),map_node_positions(end_node(n),2)];
+end
+
+% Station in bottom left of grid is closed
+nodes{2}.message_to_transmit = true;
+new_exit_point = 4;
+
+% Set initial values for each moving node e.g. start point, end point etc.
+for m = 1:number_of_moving_groups
+    for n = 1:nodes_per_group
+        index = n + (m-1)*nodes_per_group + number_of_stationary_nodes;
+        
+        nodes{index} = FloodingNode;
+        nodes{index}.id = index;
+        nodes{index}.group = m;
+        nodes{index}.start_point = [map_node_positions(start_node(m),1),map_node_positions(start_node(m),2)];
+        nodes{index}.end_point = [map_node_positions(end_node(m),1),map_node_positions(end_node(m),2)];
+        nodes{index}.min_speed = min_speed(m);
+        nodes{index}.max_speed = max_speed(m);
+        nodes{index}.location_table{1,number_of_nodes} = [];
+        nodes{index}.message_table{1,number_of_nodes} = [];
+        nodes{index}.current_position = nodes{index}.start_point;
+
+        % Positions of stationary nodes are fixed and therefore known at start
+        for k=1:number_of_stationary_nodes
+            nodes{index}.location_table{k} = nodes{k}.current_position;
+        end
+    end
+end
+clear m n k index

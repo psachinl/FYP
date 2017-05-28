580c2712a1c30a3876c7d673dd1584e70418467d
Sachin Leelasena <psachinl@users.noreply.github.com>
2017-05-28 22:29:35 +0100

    Updated scripts for batch running and early exit

--- Transmission/lmr.m
+++ Transmission/lmr.m
@@ -2,28 +2,30 @@ clear
 close all
 tic;
 
-number_of_moving_groups = 3;
-nodes_per_group = 2;
-number_of_stationary_nodes = 3;
+% Get simulation parameters
+params = getSimulationParams;
+number_of_moving_groups = params{1};
+nodes_per_group = params{2};
+number_of_stationary_nodes = params{3};
+edge_start_points = params{4};
+edge_end_points = params{5};
+edge_weights = params{6};
+start_node = params{7};
+end_node = params{8};
+min_speed = params{9};
+max_speed = params{10};
+map_node_positions = params{11};
+max_time = params{12};
+debug = params{13};
+plot_path = params{14};
+print_timing_result = params{15};
+print_power_result = params{16};
+quit_simulation_early = params{17};
+
 number_of_moving_nodes = number_of_moving_groups * nodes_per_group;
 number_of_nodes = number_of_stationary_nodes + number_of_moving_nodes;
-max_time = 400;
-debug = true; % If true, text printed to console
-plot_path = false; % If true, node paths are plotted
-
-% If result flag is true, final result is printed to console
-print_timing_result = true;
-print_power_result = false;
 
 nodes{1,number_of_nodes} = []; % Cell array to store all nodes
-edge_start_points = [1 3 3 2 6 1 7 4 7 8];
-edge_end_points =   [3 4 5 6 7 2 6 5 8 7];
-edge_weights = [579 40 128 267 163 250 0 115 18 0];
-start_node = [1,2,1,2]; % Array of start points for each group
-end_node = [4,8,5,8]; % End points
-min_speed=[1,2,0.8,2.5]; % Min and max speeds for each group
-max_speed=[2,3,1.4,2.5];
-map_node_positions = [340,440; 267,181; 340,919; 360,1000; 400,1000; 0,181; 0,18; 0,0];
 
 % Set initial values for each stationary node
 stationary_group = 0;
@@ -151,6 +153,12 @@ for t=1:max_time-1
         nodes{n}.current_position = [nodes{n}.position{4}(t+1,1),nodes{n}.position{4}(t+1,2)];
     end
     [first_transmission_time,last_transmission_time] = storeTransmissionTimes(nodes,test_group,number_of_stationary_nodes,nodes_per_group,first_transmission_time,last_transmission_time,t);
+    if last_transmission_time > 0 && quit_simulation_early
+        fprintf('Ending simulation early, time = %d \n',t);
+        [group_transmission_time,group_power_consumption] = getSimulationResults(print_timing_result,print_power_result,test_group,first_transmission_time,last_transmission_time);
+        toc;
+        return
+    end
 end
 
 clear t n k

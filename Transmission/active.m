d5fbea6f79fa946933776fafde65f9cb6370edad
Sachin Leelasena <psachinl@users.noreply.github.com>
2017-05-28 22:18:57 +0100

    Changed active test script to fetch parameters at the start of each run, wrote function to define and return simulation parameters

--- Transmission/active.m
+++ Transmission/active.m
@@ -2,32 +2,30 @@ clear
 close all
 tic;
 
-number_of_moving_groups = 3;
-nodes_per_group = 200;
-number_of_stationary_nodes = 3;
+% Get simulation parameters
+params = getSimulationParams;
+number_of_moving_groups = params{1};
+nodes_per_group = params{2};
+number_of_stationary_nodes = params{3};
+edge_start_points = params{4};
+edge_end_points = params{5};
+edge_weights = params{6};
+start_node = params{7};
+end_node = params{8};
+min_speed = params{9};
+max_speed = params{10};
+map_node_positions = params{11};
+max_time = params{12};
+debug = params{13};
+plot_path = params{14};
+print_timing_result = params{15};
+print_power_result = params{16};
+quit_simulation_early = params{17};
+
 number_of_moving_nodes = number_of_moving_groups * nodes_per_group;
 number_of_nodes = number_of_stationary_nodes + number_of_moving_nodes;
-max_time = 300;
-debug = false; % If true, text printed to console
-plot_path = false; % If true, initial node paths are plotted
-
-% If result flag is true, final result is printed to console
-print_timing_result = true;
-print_power_result = false;
-
-% If quit flag is true, the simulation will exit once all nodes in the test
-% group have recieved the message
-quit_simulation_early = true;
 
 nodes{1,number_of_nodes} = []; % Cell array to store all nodes
-edge_start_points = [1 3 3 2 6 1 7 4 7 8];
-edge_end_points =   [3 4 5 6 7 2 6 5 8 7];
-edge_weights = [579 40 128 267 163 250 0 115 18 0];
-start_node = [1,2,1,2]; % Array of start points for each group
-end_node = [4,8,5,8]; % End points
-min_speed=[1,2,0.8,2.5]; % Min and max speeds for each group
-max_speed=[2,3,1.4,2.5];
-map_node_positions = [340,440; 267,181; 340,919; 360,1000; 400,1000; 0,181; 0,18; 0,0];
 
 % Set initial values for each stationary node
 stationary_group = 0;

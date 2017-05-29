206c065148abab1c233787cbf11643141660383f
Sachin Leelasena <psachinl@users.noreply.github.com>
2017-05-29 15:58:42 +0100

    Fixed indexing error with upper limit of for loop

--- Transmission/getNumberOfNodesWithMessage.m
+++ Transmission/getNumberOfNodesWithMessage.m
@@ -2,14 +2,14 @@ function num_nodes = getNumberOfNodesWithMessage(nodes,test_group,number_of_stat
     % Function that returns the number of nodes in a test group which have
     % received the message
     
-    % Start node and end node are the node id's of the first and last nodes
-    % within the test group
-    start_node = number_of_stationary_nodes + (test_group-1) * nodes_per_group + 1;
-    end_node = start_node + nodes_per_group;
+    % Start index and end index are the node id's of the first and last 
+    % nodes within the test group
+    start_index = number_of_stationary_nodes + (test_group-1) * nodes_per_group + 1;
+    end_index = start_index + nodes_per_group - 1;
     
     num_nodes = 0; % Initialise number of nodes with message to 0
     
-    for i=start_node:end_node
+    for i=start_index:end_index
         if nodes{i}.message_to_transmit
             num_nodes = num_nodes + 1;
         end

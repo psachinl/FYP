930cf96b81e351ab8f36bd40fa7dd7a6990a1fbc
Sachin Leelasena <psachinl@users.noreply.github.com>
2017-06-14 19:21:22 +0100

    Removed old code, improved variable names

--- Mobility/plotshortestpath_test.m
+++ Mobility/plotshortestpath_test.m
@@ -1,23 +1,14 @@
 clear
-% W = [.41 .99 .51 .32 .15 .45 .38 .32 .36 .29 .21];
-% W = [51 15 45 38 36 29 12];
-% DG = sparse([6 1 2 2 3 4 4 5 5 6 1],[2 6 3 5 4 1 6 3 4 3 5],W); % Directed graph
-% s = [2 3 4 5 5 6 1];
-% e = [3 4 1 6 4 3 2];
-s = [1 3 3 2 6 1 7 4 7 8];
-e = [3 4 5 6 7 2 6 5 8 7];
-W = [579 40 128 267 163 250 0 115 18 0];
-DG = sparse(s,e,W);
+close all
+
+start_points = [1 3 3 2 6 1 7 4 7 8]; % Edge start points
+end_points = [3 4 5 6 7 2 6 5 8 7]; % Edge end points
+Weights = [579 40 128 267 163 250 0 115 18 0]; % Edge weights
+DG = sparse(start_points,end_points,Weights); % Directed Graph
 % First vector shows start point for edge, second shows end point e.g. edge
 % from node 6 to node 2. W shows the weight of each edge
-% h = view(biograph(DG,[],'ShowWeights','on'));
-% [dist,path,pred] = graphshortestpath(DG,1,6)
-% set(h.Nodes(path),'Color',[1 0.4 0.4])
-% edges = getedgesbynodeid(h,get(h.Nodes(path),'ID'));
-% set(edges,'LineColor',[1 0 0])
-% set(edges,'LineWidth',1.5)
 
-UG = tril(DG + DG')
+UG = tril(DG + DG') % Generate Undirected Graph
 h = view(biograph(UG,[],'ShowArrows','off','ShowWeights','on'))
 [dist,path,pred] = graphshortestpath(UG,1,4,'directed',false)
 set(h.Nodes(path),'Color',[1 0.4 0.4]) % Changes the colour of the nodes on the shortest path
@@ -27,10 +18,10 @@ edges = [fowEdges;revEdges];
 set(edges,'LineColor',[1 0 0])
 set(edges,'LineWidth',1.5)
 
-[dist1,path1,pred1] = graphshortestpath(UG,2,8,'directed',false)
-set(h.Nodes(path1),'Color',[0.4 1 0.4]) % Changes the colour of the nodes on the shortest path
-fowEdges1 = getedgesbynodeid(h,get(h.Nodes(path1),'ID'));
-revEdges1 = getedgesbynodeid(h,get(h.Nodes(fliplr(path1)),'ID'));
-edges1 = [fowEdges1;revEdges1];
-set(edges1,'LineColor',[0 1 0])
-set(edges1,'LineWidth',1.5)
+% [dist1,path1,pred1] = graphshortestpath(UG,2,8,'directed',false)
+% set(h.Nodes(path1),'Color',[0.4 1 0.4]) % Changes the colour of the nodes on the shortest path
+% fowEdges1 = getedgesbynodeid(h,get(h.Nodes(path1),'ID'));
+% revEdges1 = getedgesbynodeid(h,get(h.Nodes(fliplr(path1)),'ID'));
+% edges1 = [fowEdges1;revEdges1];
+% set(edges1,'LineColor',[0 1 0])
+% set(edges1,'LineWidth',1.5)
